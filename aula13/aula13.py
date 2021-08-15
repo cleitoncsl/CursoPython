b11a9687b243a6f9e958e42cbd8e474a5e4ed7ea
Cleiton da Silva Leal <cleiton_csl@live.com>
2021-08-15 17:45:43 -0300

    aulas de format

--- aula13/aula13.py
+++ aula13/aula13.py
@@ -21,6 +21,20 @@ print(f'{numero2:0>3}')
 print(f'{numero2:0<3}')
 print(f'{numero2:0>10.2f}')
 print(f'{numero2:0<10.2f}')
-nome = 'Otavio Miranda'
+nome = 'Otavio'
+sobrenome = 'Miranda'
 print((50-len(nome)) / 2)
 print(f'{nome:#^50}')
+nome_formatado = '{:@>19}'.format(nome)
+nome_formatado1 = '{:@<19}'.format(nome)
+nome_formatado2 = '{:@^20}'.format(nome)
+sobre_nome = '{1:#^10}'.format(nome,sobrenome)
+sobre_nome2 = '{0:#^10}' '{1:@^50}'.format(nome,sobrenome)
+print(nome_formatado)
+print(nome_formatado1)
+print(nome_formatado2)
+print(sobre_nome)
+print(sobre_nome2)
+
+nome = 'otavio miranda'
+print(nome.title())
