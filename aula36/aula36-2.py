0c3b48d34a0edfee3cf868e7908e3d0c3e388aab
cleitoncsl <cleitoncsl@gmail.com>
2021-09-12 19:56:02 -0300

    VER: 12092021 1955

--- aula36/aula36-2.py
+++ aula36/aula36-2.py
@@ -1,7 +1,6 @@
-cpf = '16899535009'
+cpf = input("Digite um CPF: ")
 novo_cpf = cpf[:-2]
-#print(novo_cpf)
-#print(f'--{novo_cpf}--')
+print(f'CPF digitado: {cpf}')
 reverso = 10
 total = 0
 
@@ -10,6 +9,18 @@ for index in range(19):
         index -= 9
 
     total += int(novo_cpf[index]) * reverso
+
     reverso -= 1
     if reverso < 2:
         reverso = 11
+        d = 11 - (total % 11)
+
+        if d > 9:
+            d = 0
+        total = 0
+        novo_cpf += str(d)
+
+if(cpf == novo_cpf):
+    print('CPF válido')
+else:
+    print('CPF inválido')
