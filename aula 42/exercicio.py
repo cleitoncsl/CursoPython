ff93b2cc1d58842bf11317a9a00ee3f6ab65fce1
cleitoncsl <cleitoncsl@gmail.com>
2022-04-14 21:50:06 -0300

    65. Exercicio de list comprehession

--- aula 42/exercicio.py
+++ aula 42/exercicio.py
@@ -1,5 +1,10 @@
-l1 = ['01234567890123456789012345678901234567890123456789']
+l1 = '01234567890123456789012345678901234567890123456789'
+
+lista = [letra for letra in l1]
+n = 10
+#comp = [(i, i + n) for i in range(0, len(l1), n)]
+#comp = [i for i in range(0, len(l1), n)]
+comp = [l1[i:i + n] for i in range(0, len(l1), n)]
 
 
-
-print(l1)
+print(comp)
